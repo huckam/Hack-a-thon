8a3b63a7677d532e1f173958d059ef8e15e7ef11
Anthony Huck <anthony.huck@cgi.com>
2023-11-06 15:02:22 -0500

    added data and infrastructure service registration. added swagger/swaggerUI

--- ECTPFinalProject/ECTPFinalProject.API/Program.cs
+++ ECTPFinalProject/ECTPFinalProject.API/Program.cs
@@ -1,7 +1,15 @@
+using ECTPFinalProject.Data.Helpers;
+using ECTPFinalProject.Infrastructure.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Modify this in appsettings.json to connect to your sql database
+builder.Services.AddEntityFrameworkRepositories(builder.Configuration.GetConnectionString("SampleDatabase"));
+
+builder.Services.AddInfrastructureServices();
 
 var app = builder.Build();
 
@@ -11,6 +19,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
