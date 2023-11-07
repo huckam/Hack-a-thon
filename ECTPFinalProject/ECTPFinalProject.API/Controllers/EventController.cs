8f66558f7771a246cc8a4937e003e193005ba88c
Zaren Wienclaw <zaren.wienclaw@cgi.com>
2023-11-07 16:59:41 -0500

    Add angular default project

---
+++ ECTPFinalProject/ECTPFinalProject.API/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using ECTPFinalProject.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECTPFinalProject.API.Controllers;
+
+[ApiController]
+[Route("events")]
+public class EventController : ControllerBase
+{
+    private readonly ILogger<EventController> _logger;
+
+    public EventController(ILogger<EventController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Event> Get()
+    {
+        var list = new List<Event>();
+        return list;
+    }
+}
---
+++ ECTPFinalProject/ECTPFinalProject.Website/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public events: Event[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Event[]>(baseUrl + 'events').subscribe(result => {
+      this.events = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Event {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
