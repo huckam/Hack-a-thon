90678832bc83dcb4e555bb9c2ed980405aaddab1
Patrick Dewey <38944718@id.ohio.gov>
2023-11-12 16:42:27 -0500

    FE chnages

--- ECTPFinalProject/FEApp/golf-league-app/src/app/app.module.ts
+++ ECTPFinalProject/FEApp/golf-league-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberPageComponent } from './member-page/member-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberPageComponent
   ],
   imports: [
     BrowserModule,
